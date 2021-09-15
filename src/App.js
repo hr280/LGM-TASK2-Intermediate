442bf400e1d068b0d10e1f0b56a29f871311ba98
Hassan Raza <64700946+hr280@users.noreply.github.com>
2021-09-15 16:58:28 +0500

    Task2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import Users from "./cards";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react'
+	
+class App extends Component {
+  constructor(props){
+	super(props)
+		
+	// Set initial state
+	this.state = {users_data :[],
+                loading : true                
+  }
+  
 
+	this.updateState = this.updateState.bind(this)
+  }
+    
+  updateState(){
+      document.getElementById("main").style.display='flex';
+      const link="https://reqres.in/api/users?page=1";
+      fetch(link)
+      .then(response => response.json())
+      .then((users) => {
+        
+        this.setState({users_data :users.data,
+                        loading: false
+        })
+        console.log(users.data);
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+    
+  render(){
+    return (<>
+    
+      <nav className="navbar navbar-expand-lg navbar-light">
+          <div className="container-fluid">
+            <h3 class="first-h2"> <span>#HASH</span>Coder</h3>
+            <button onClick={this.updateState}> Users</button>
+          </div>
+        </nav>
+      <Users loading={this.state.loading} users={this.state.users_data}/>
+    </>
+    )
+  }
+}
+	
 export default App;
